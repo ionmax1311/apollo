1a8983dc97204593255f376f82ec6a8b9b3c7811
Пользователь <ionmax1311@gmail.com>
2021-06-02 09:16:28 +0300

    1.07

--- dist/js/index.js
+++ dist/js/index.js
@@ -67,8 +67,16 @@ $(document).ready(function () {
 			},
 		},
 	});
+	$("#payments-table").DataTable({
+		info: false,
+		language: {
+			paginate: {
+				previous: "<",
+				next: ">",
+			},
+		},
+	});
 });
-
 
 // $("#dashboard-table_wrapper>.row:nth-child(3)").append("<div>more</div>");
 
@@ -77,7 +85,56 @@ $(document).ready(function () {
 // 	$(this).addClass('active')
 // })
 
+$(document).ready(function () {
+	console.log($(".wrapper").hasClass("dashboard-active"));
+	if ($(".wrapper").hasClass("dashboard-active")) {
+		$(".inner a").removeClass("active");
+		$(".inner").find('a:contains("Dashboard")');
 
+		console.log(1);
+	} else if ($(".wrapper").hasClass("order-history-active")) {
+		$(".inner a").removeClass("active");
+		$(".inner").find('a:contains("Order History")');
+
+		console.log(2);
+	}
+});
+
+$(".slider-card").slick({
+	// autoplay: true,
+	// autoplaySpeed: 1000,
+	// asNavFor: ".slider-poster-nav",
+	// centerPadding: "60px",
+	// slidesToShow: 4,
+	// slidesToScroll: 1,
+	dots: false,
+	arrows: true,
+	infinite: true,
+	appendArrows: ".card-slider-arrows",
+	variableWidth: true,
+	prevArrow:
+		'<button id="prev" type="button" class=" btn-slider-card"><img src="img/arrow-sl-left.png" alt=""></button>',
+	nextArrow:
+		'<button id="next" type="button" class=" btn-slider-card"><img src="img/arrow-sl-right.png" alt=""></button>',
+	responsive: [
+		{
+			breakpoint: 1300,
+			settings: {
+				// arrows: true,
+
+				// centerPadding: "40px",
+				slidesToShow: 3,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				arrows: true,
+				slidesToShow: 2,
+			},
+		},
+	],
+});
 
 /*
  Copyright (C) Federico Zivolo 2019
